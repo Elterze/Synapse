9ebb1fd286528f308453d3438dc9184626769a04
Elterze <42221555+Elterze@users.noreply.github.com>
2018-08-17 23:48:10 +0200

    add Celery worker for periodic offense polling + Flask endpoints to start and stop it

--- app.py
+++ app.py
@@ -4,13 +4,19 @@
 import os
 import logging, logging.handlers
 from flask import Flask, request, jsonify
+from celery import Celery
+from celery.task.control import inspect
 import json
+import time
 
 from workflows.common.common import getConf
 from workflows.Ews2Case import connectEws
 from workflows.qradartest import createQradarAlert, closingQradarOffense
 
 app_dir = os.path.dirname(os.path.abspath(__file__))
+
+#get configuration from configuration file
+cfg = getConf()
 
 #create logger
 logger = logging.getLogger('workflows')
@@ -31,8 +37,34 @@ if not logger.handlers:
     #Adding the file handler
     logger.addHandler(file_handler)
 
+#create Flask app
 app = Flask(__name__)
 
+#create Celery worker
+celery = Celery(app.name, broker=cfg.get('Celery', 'broker'), backend=cfg.get('Celery', 'backend'))
+celery.conf.update(app.config)
+
+@celery.task(bind=True, name="QradarPeriodic")
+def qradarperiodictask(self, interval):
+    """Start a Celery worker launching createQradarAlert every interval seconds"""
+    def is_revoked():
+        i = celery.control.inspect()
+        revoked_tasks = list(i.revoked().values())[0]
+        for task in revoked_tasks :
+            if task == self.request.id :
+                return True
+            else :
+                return False
+
+    self.update_state(state='RUNNING')
+    while not is_revoked() :
+        print("Fetching QRadar offenses...")
+        createQradarAlert()
+        print("Completed!")
+        time.sleep(interval)
+    return {'status': 'Task completed!'}
+
+#create Flask endpoints
 @app.route('/ews2case',methods=['GET'])
 def ews2case():
     workflowReport = connectEws()
@@ -48,6 +80,32 @@ def qradaralert():
         return jsonify(workflowReport), 200
     else:
         return jsonify(workflowReport), 500
+
+@app.route('/launchqradartask', methods=['GET'])
+def launch_task():
+    task = qradarperiodictask.apply_async((30,))
+    if task.state == "RUNNING" or task.state == "PENDING":
+        return jsonify({'sucess': True}), 200
+    else:
+        return jsonify({'sucess': False}), 500
+
+@app.route('/revokeqradartask', methods=['GET'])
+def revoke():
+    report = dict()
+    report['success'] = False
+    i = celery.control.inspect()
+    running_tasks = list(i.active().values())[0]
+    for task in running_tasks:
+        if task['name'] == "QradarPeriodic":
+            worker = qradarperiodictask.AsyncResult(task['id'])
+            worker.revoke(wait=True, timeout=5)
+            print(task['name'] + " worker with id  " + task['id'] +
+            " has been revoked")
+            report['success'] = True
+    if report['success']:
+        return jsonify(report), 200
+    else:
+        return jsonify(report), 500
 
 @app.route('/thehivewebhook',methods=['POST'])
 def thehivewebhook():
@@ -87,7 +145,6 @@ def getSynapseVersion():
     return jsonify({'version': '1.0'}), 200
 
 if __name__ == '__main__':
-    cfg = getConf()
     app.run(debug=cfg.getboolean('api', 'debug'),
         host=cfg.get('api', 'host'),
         port=cfg.get('api', 'port'),
